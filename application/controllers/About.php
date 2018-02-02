2c7518e2a25707856bb1eb17511bf715e21154da
ayspiritnolimit <arisyuda4215@gmail.com>
2018-02-02 10:18:36 +0700

    Revert "coba"
    This reverts commit 19dad0dba5e5b132bd402b8e4abb78efca94a19f.

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class About gfg extends CI_Controller {
+class About extends CI_Controller {
 	
 	public function __construct() {
 		parent::__construct();
