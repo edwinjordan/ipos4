f6337b5473f3017eb2ff50205a40dbb7d9524bb5
ayspiritnolimit <arisyuda4215@gmail.com>
2018-02-06 17:01:01 +0700

    item baru

--- application/controllers/Item_Baru.php
+++ application/controllers/Item_Baru.php
@@ -1,20 +1,111 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Item_Baru extends CI_Controller {
+class Item_Baru extends CI_Controller
+{
 
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('Mdl_itembaru');
-		$this->auth->restrict();
-		date_default_timezone_set("Asia/Jakarta");
-		$this->load->library("session");
-	}
+    public function __construct()
+    {
+        parent::__construct();
+//        $this->load->model('Mdl_itembaru');
+        $this->load->model('Mdl_itembaru_dataumum');
+        $this->auth->restrict();
+        date_default_timezone_set("Asia/Jakarta");
+        $this->load->library("session");
+    }
 
-	function index(){
-       // $this->mdl_home->getsqurity();
-        $data['view_file']    = "item_baru/view_item_baru";
-        $this->load->view('admin_view',$data);
+    function index()
+    {
+        // $this->mdl_home->getsqurity();
+        $data['view_file'] = "item_baru/view_item_baru";
+        $this->load->view('admin_view', $data);
+    }
+
+//    public function ajax_list()
+//    {
+//        $list = $this->Mdl_itembaru_dataumum->get_datatables();
+//        $data = array();
+////        $no = $_REQUEST['start'];
+//        foreach ($list as $dataumum) {
+////            $no++;
+//            $row = array();
+////            $row[] = $no;
+//            $row[] = $dataumum->kode_item_tipe;
+//            $row[] = $dataumum->kode_item_jenis;
+//            $row[] = $dataumum->kode_merek;
+//            $row[] = $dataumum->kode_gudang;
+//            $row[] = $dataumum->kode_rak;
+//            $row[] = $dataumum->item_nama;
+//            $row[] = $dataumum->item_ket;
+//            $row[] = $dataumum->item_status_jual;
+//            $row[] = $dataumum->item_stok_min;
+//
+//            $row[] = '
+//			<div class="btn-group">
+//                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Aksi <span class="caret"></span></button>
+//                        <ul class="dropdown-menu" role="menu">
+//                            <li><a href="javascript:void(0)" onclick="edit(' . "'" . $dataumum->kode_item . "'" . ')">Edit</a></li>
+//                            <li><a href="javascript:void(0)" onclick="hapus(' . "'" . $dataumum->kode_item . "'" . ')">Delete</a></li>
+//                        </ul>
+//            </div>';
+//            $data[] = $row;
+//        }
+//
+//        $output = array(
+//            "draw" => $_REQUEST['draw'],
+//            "recordsTotal" => $this->Mdl_itembaru_dataumum->count_all(),
+//            "recordsFiltered" => $this->Mdl_itembaru_dataumum->count_filtered(),
+//            "data" => $data,
+//        );
+//        echo json_encode($output);
+//    }
+
+    public function ajax_add()
+    {
+        $data = array(
+            'kode_item_tipe' => $this->input->post('kode_item_tipe'),
+            'kode_item_jenis' => $this->input->post('kode_item_jenis'),
+            'kode_merek' => $this->input->post('kode_merek'),
+            'kode_gudang' => $this->input->post('kode_gudang'),
+            'kode_rak' => $this->input->post('kode_rak'),
+            'item_nama' => $this->input->post('item_nama'),
+            'item_ket' => $this->input->post('item_ket'),
+            'item_status_jual' => $this->input->post('item_status_jual'),
+            'item_stok_min' => $this->input->post('item_stok_min'),
+
+
+        );
+        $insert = $this->Mdl_itembaru_dataumum->add($data);
+        echo json_encode(array('status' => TRUE));
+    }
+
+    public function ajax_edit($id)
+    {
+        $data = $this->Mdl_itembaru_dataumum->get_by_id($id);
+        echo json_encode($data);
+    }
+
+    public function ajax_update()
+    {
+        $data = array(
+            'kode_item_tipe' => $this->input->post('kode_item_tipe'),
+            'kode_item_jenis' => $this->input->post('kode_item_jenis'),
+            'kode_merek' => $this->input->post('kode_merek'),
+            'kode_gudang' => $this->input->post('kode_gudang'),
+            'kode_rak' => $this->input->post('kode_rak'),
+            'item_nama' => $this->input->post('item_nama'),
+            'item_ket' => $this->input->post('item_ket'),
+            'item_status_jual' => $this->input->post('item_status_jual'),
+            'item_stok_min' => $this->input->post('item_stok_min'),
+        );
+        $this->Mdl_itembaru_dataumum->update(array('kode_item' => $this->input->post('kode_item')), $data);
+        echo json_encode(array("status" => TRUE));
+    }
+
+    public function ajax_delete($id)
+    {
+        $this->Mdl_itembaru_dataumum->delete_by_id($id);
+        echo json_encode(array("status" => TRUE));
     }
 
 }		
--- application/views/item_baru/view_item_baru.php
+++ application/views/item_baru/view_item_baru.php
@@ -89,7 +89,11 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div>
-                                    <button class="btn btn-primary" type="button"><i class="fa fa-save"></i> Save</button><span> </span>
+<!--                                    <button class="btn btn-primary" type="button"><i class="fa fa-save"></i> Save</button><span> </span>-->
+                                    <button class="btn btn-info" type="button" id="btn_save" onclick="save()">
+                                        <i class="ace-icon fa fa-check bigger-110"></i>
+                                        Submit
+                                    </button>
                                     <button class="btn btn-warning" type="button"><i class="fa fa-close"></i> Batal</button>
                                 </div>
                             </div>
@@ -101,7 +105,12 @@
         </div>
 </div>
 
-<script>
+<script type="text/javascript">
+    var zonk = '';
+    var save_method;
+    var link = "<?php echo site_url('Item_Baru')?>";
+    var table;
+
     $(document).ready(function () {
         //$('#idImgLoader').show(2000);
         $('#idImgLoader').fadeOut(2000);
@@ -112,5 +121,121 @@
 
     function data() {
         $('#data').fadeIn();
+    }
+
+    $(document).ready(function () {
+        table = $('#dynamic-table').DataTable({
+
+            "processing": true, //Feature control the processing indicator.
+            "serverSide": true, //Feature control DataTables' server-side processing mode.
+            "bDestroy": true,
+            "order": [], //Initial no order.
+
+            // Load data for the table's content from an Ajax source
+            "ajax": {
+                "url": "<?php echo site_url('Item_Baru/ajax_list')?>",
+                "type": "POST"
+            },
+
+            //Set column definition initialisation properties.
+            "columnDefs": [
+                {
+                    "targets": [-1], //last column
+                    "orderable": false, //set not orderable
+                },
+            ],
+
+        });
+
+    }).fnDestroy();
+
+
+    function Tambah() {
+        save_method = 'add';
+        $('#panel-data').fadeOut('slow');
+        $('#form-data').fadeIn('slow');
+        document.getElementById('formAksi').reset();
+    }
+
+    function reload_table() {
+        table.ajax.reload(null, false);
+    }
+
+    function Batal() {
+        $('#form-data').slideUp(500,'swing');
+        $('#panel-data').fadeIn(1000);
+        location.reload();
+    }
+
+    function save() {
+        $('#btn_save').text('Saving...');
+        $('#btn_save').attr('disabled', true);
+
+        var url;
+        if (save_method == 'add') {
+            url = link + "/ajax_add";
+        } else {
+            url = link + "/ajax_update";
+        }
+        tinyMCE.triggerSave();
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: $('#formAksi').serialize(),
+            dataType: "JSON",
+            success: function (result) {
+                if (result.status) {
+
+                    setTimeout(function () {
+                        Batal();
+                    }, 1000);
+
+                    setTimeout(function () {
+                        reload_table();
+                    }, 1000);
+                }
+                setTimeout(function () {
+                    $('#btn_save').text('Save');
+                    $('#btn_save').attr('disabled', false);
+                    document.getElementById('formAksi').reset();
+                }, 1000);
+                swal_berhasil();
+                setTimeout(function () {
+                    reload_table();
+                }, 1000);
+            }, error: function (jqXHR, textStatus, errorThrown) {
+                // alert('Error adding/update data');
+                swal({title: "ERROR", text: "Error adding / update data", type: "warning", closeOnConfirm: true});
+                $('#btnSave').text('save');
+                $('#btnSave').attr('disabled', false);
+            }
+        });
+    }
+
+    function edit(id) {
+        save_method = 'update';
+        $('#panel-data').fadeOut('slow');
+        $('#form-data').fadeIn('slow');
+        document.getElementById('formAksi').reset();
+        $.ajax({
+            url : link+"/ajax_edit/"+id,
+            type: "GET",
+            dataType: "JSON",
+            success: function(result) {
+                //document.getElementById('fc_kdbahan').setAttribute('readonly','readonly');
+                $('[name="kode_item"]').val(result.kode_item);
+                $('[name="kode_item_tipe"]').val(result.kode_item_tipe);
+                $('[name="kode_merek"]').val(result.kode_merek);
+                $('[name="kode_gudang"]').val(result.kode_gudang);
+                $('[name="kode_rak"]').val(result.kode_rak);
+                $('[name="item_nama"]').val(result.item_nama);
+                $('[name="item_ket"]').val(result.item_ket);
+                $('[name="item_status_jual"]').val(result.item_status_jual);
+                $('[name="item_stok_min"]').val(result.item_stok_min);
+
+            }, error: function (jqXHR, textStatus, errorThrown) {
+                alert('Error get data from ajax');
+            }
+        });
     }
 </script>
