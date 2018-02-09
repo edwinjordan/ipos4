3053f12cc084bda6abc9db9222cf02b9ea5fa4cf
ayspiritnolimit <arisyuda4215@gmail.com>
2018-02-09 08:36:23 +0700

    fix controller master data

--- application/controllers/Bank.php
+++ application/controllers/Bank.php
@@ -3,13 +3,77 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bank extends CI_Controller {
 
-	public function __construct() {
-		parent::__construct();
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Mdl_bank');
+        $this->auth->restrict();
+        date_default_timezone_set("Asia/Jakarta");
+        $this->load->library("session");
+    }
 
-	function index(){
-       // $this->mdl_home->getsqurity();
+    function index(){
         $data['view_file']    = "bank/view_bank";
         $this->load->view('admin_view',$data);
-    }
+        }
+
+	public function ajax_add() {
+            $data = array(
+                'kode_bank'         => $this->input->post('kode_bank'),
+                'bank_nama' 	     => $this->input->post('bank_nama'),
+                'no_rek'       		 => $this->input->post('no_rek'),
+            );
+            $insert = $this->Mdl_bank->add($data);
+            //print_r($this->db->last_query());
+            echo json_encode(array('status' => TRUE));
+        }
+
+	public function ajax_edit($id) {
+            $data = $this->Mdl_bank->get_by_id($id);
+            echo json_encode($data);
+        }
+
+	public function ajax_update() {
+            $data = array(
+                'kode_bank'         => $this->input->post('kode_bank'),
+                'bank_nama' 	     => $this->input->post('bank_nama'),
+                'no_rek'       		 => $this->input->post('no_rek'),
+            );
+            $this->Mdl_bank->update(array('kode_bank' => $this->input->post('kode_bank')), $data);
+            echo json_encode(array("status" => TRUE));
+        }
+
+	public function ajax_delete($id) {
+            $this->Mdl_bank->delete_by_id($id);
+            echo json_encode(array("status" => TRUE));
+        }
+
+     public function ajax_list() {
+            $list = $this->Mdl_bank->get_datatables();
+            $data = array();
+            $no = $_REQUEST['start'];
+            foreach ($list as $bank) {
+                $no++;
+                $row = array();
+                $row[] = $bank->kode_bank;
+                $row[] = $bank->bank_nama;
+                $row[] = $bank->no_rek;
+                $row[] = '
+			<div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Aksi <span class="caret"></span></button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="javascript:void(0)" onclick="edit('."'".$bank->kode_bank."'".')">Edit</a></li>
+                            <li><a href="javascript:void(0)" onclick="hapus('."'".$bank->kode_bank."'".')">Delete</a></li>
+                        </ul>
+            </div>';
+                $data[] = $row;
+            }
+
+            $output = array(
+                "draw" => $_REQUEST['draw'],
+                "recordsTotal" => $this->Mdl_bank->count_all(),
+                "recordsFiltered" => $this->Mdl_bank->count_filtered(),
+                "data" => $data,
+            );
+            echo json_encode($output);
+        }
 }		
--- application/controllers/Grup_Pelanggan.php
+++ application/controllers/Grup_Pelanggan.php
@@ -3,13 +3,82 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Grup_Pelanggan extends CI_Controller {
 
-	public function __construct() {
-		parent::__construct();
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Mdl_gruppelanggan');
+        $this->auth->restrict();
+        date_default_timezone_set("Asia/Jakarta");
+        $this->load->library("session");
+    }
 
-	function index(){
-       // $this->mdl_home->getsqurity();
+    function index(){
+        // $this->mdl_home->getsqurity();
+        $data['view_file']    = "Grup_Pelanggan/view_grup_pelanggan";
         $data['view_file']    = "grup_pelanggan/view_grup_pelanggan";
         $this->load->view('admin_view',$data);
     }
-}		
+    public function ajax_add() {
+        $data = array(
+            'kode_plg_grup'         => $this->input->post('kode_plg_grup'),
+            'plg_grup_nama' 	     => $this->input->post('plg_grup_nama'),
+            'plg_grup_potongan'       		 => $this->input->post('plg_grup_potongan'),
+            'plg_grup_level'        	 => $this->input->post('plg_grup_level'),
+
+        );
+        $insert = $this->Mdl_gruppelanggan->add($data);
+        //print_r($this->db->last_query());
+        echo json_encode(array("status" => TRUE));
+    }
+    public function ajax_edit($id) {
+        $data = $this->Mdl_gruppelanggan->get_by_id($id);
+        echo json_encode($data);
+    }
+
+    public function ajax_update() {
+        $data = array(
+            'kode_plg_grup'         => $this->input->post('kode_plg_grup'),
+            'plg_grup_nama' 	     => $this->input->post('plg_grup_nama'),
+            'plg_grup_potongan'       		 => $this->input->post('plg_grup_potongan'),
+            'plg_grup_level'        	 => $this->input->post('plg_grup_level'),
+        );
+        $this->Mdl_gruppelanggan->update(array('kode_plg_grup' => $this->input->post('kode_plg_grup')), $data);
+        echo json_encode(array("status" => TRUE));
+    }
+
+    public function ajax_delete($id) {
+        $this->Mdl_gruppelanggan->delete_by_id($id);
+        echo json_encode(array("status" => TRUE));
+    }
+
+    public function ajax_list() {
+        $list = $this->Mdl_gruppelanggan->get_datatables();
+        $data = array();
+        $no = $_REQUEST['start'];
+        foreach ($list as $grup) {
+            $no++;
+            $row = array();
+            $row[] = $grup->kode_plg_grup;
+            $row[] = $grup->plg_grup_nama;
+            $row[] = $grup->plg_grup_potongan;
+            $row[] = $grup->plg_grup_level;
+            $row[] = '
+			<div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Aksi <span class="caret"></span></button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="javascript:void(0)" onclick="edit('."'".$grup->kode_plg_grup."'".')">Edit</a></li>
+                            <li><a href="javascript:void(0)" onclick="hapus('."'".$grup->kode_plg_grup."'".')">Delete</a></li>
+                        </ul>
+            </div>';
+            $data[] = $row;
+        }
+
+        $output = array(
+            "draw" => $_REQUEST['draw'],
+            "recordsTotal" => $this->Mdl_gruppelanggan->count_all(),
+            "recordsFiltered" => $this->Mdl_gruppelanggan->count_filtered(),
+            "data" => $data,
+        );
+        echo json_encode($output);
+    }
+
+}
